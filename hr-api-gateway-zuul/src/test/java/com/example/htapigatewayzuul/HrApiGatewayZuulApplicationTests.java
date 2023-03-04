808fed97da885c9be5fc8bc56c9543117069cb36
João Paulo <jpcchaves@outlook.com>
2023-03-04 11:29:36 -0300

    feat: add hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/com/example/htapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
