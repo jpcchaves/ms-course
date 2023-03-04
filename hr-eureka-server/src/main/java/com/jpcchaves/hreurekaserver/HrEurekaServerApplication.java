70011dd5afd4a65f671be1d90933bec3ccd3ea01
João Paulo <jpcchaves@outlook.com>
2023-03-04 10:45:13 -0300

    feat: add random port to hr-worker

--- hr-eureka-server/src/main/java/com/jpcchaves/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/jpcchaves/hreurekaserver/HrEurekaServerApplication.java
@@ -1,6 +1,7 @@
 package com.jpcchaves.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
